512be26a40856159ea31422510bdf4d2e69d5913
Er. Anshuman Sahoo <61726230+anshuman-sahoo1999@users.noreply.github.com>
2025-12-05 17:49:31 +0530

    Implement Burgers' equation loss and update wave loss
    Added Burgers' equation loss function and updated wave equation loss.

--- PyBayesPINN/pybayespinn/physics.py
+++ PyBayesPINN/pybayespinn/physics.py
@@ -4,7 +4,6 @@ class PhysicsUtils:
     """
     Toolkit for computing high-order derivatives automatically using PyTorch Autograd.
     """
-    
     @staticmethod
     def first_derivative(u, x):
         """Computes du/dx"""
@@ -28,34 +27,40 @@ class PhysicsUtils:
         )[0]
         return d2u_dx2
 
+# --- Model 1: Wave Equation (Linear) ---
 def wave_equation_loss(model, xt_collocation, c=1.0):
     """
-    Calculates the Physics Residual for the 1D Wave Equation:
-    u_tt - c^2 * u_xx = 0
-    
-    Args:
-        model: The neural network
-        xt_collocation: Tensor of shape [N, 2] representing (x, t) points
-        c: Wave speed constant
+    Physics Residual for the 1D Wave Equation: u_tt - c^2 * u_xx = 0
     """
-    # Enable gradient tracking for inputs
     xt_collocation.requires_grad = True
-    
-    # Forward pass
     u = model(xt_collocation)
     
-    # Compute gradients w.r.t input (x, t)
+    # Gradients
     grads = torch.autograd.grad(u, xt_collocation, torch.ones_like(u), create_graph=True, retain_graph=True)[0]
+    u_x, u_t = grads[:, 0:1], grads[:, 1:2]
     
-    # Split gradients: column 0 is x, column 1 is t
-    u_x = grads[:, 0:1]
-    u_t = grads[:, 1:2]
-    
-    # Compute second derivatives
+    # Second Derivatives
     u_xx = torch.autograd.grad(u_x, xt_collocation, torch.ones_like(u_x), create_graph=True, retain_graph=True)[0][:, 0:1]
     u_tt = torch.autograd.grad(u_t, xt_collocation, torch.ones_like(u_t), create_graph=True, retain_graph=True)[0][:, 1:2]
     
-    # The Physics Residual (Should be 0)
     residual = u_tt - (c**2 * u_xx)
+    return torch.mean(residual**2)
+
+# --- Model 2: Burgers' Equation (Non-Linear) ---
+def burgers_equation_loss(model, xt_collocation, nu=0.01/3.14159):
+    """
+    Physics Residual for Viscous Burgers' Equation: u_t + u*u_x - nu*u_xx = 0
+    """
+    xt_collocation.requires_grad = True
+    u = model(xt_collocation)
     
+    # Gradients
+    grads = torch.autograd.grad(u, xt_collocation, torch.ones_like(u), create_graph=True, retain_graph=True)[0]
+    u_x, u_t = grads[:, 0:1], grads[:, 1:2]
+    
+    # Second Derivatives
+    u_xx = torch.autograd.grad(u_x, xt_collocation, torch.ones_like(u_x), create_graph=True, retain_graph=True)[0][:, 0:1]
+    
+    # The Physics: u_t + u*u_x - nu*u_xx
+    residual = u_t + (u * u_x) - (nu * u_xx)
     return torch.mean(residual**2)
